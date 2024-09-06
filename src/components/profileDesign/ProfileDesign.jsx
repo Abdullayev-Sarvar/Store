79128fa7c76bb4bd326a0a2c1e93c7cf8d1ae777
Abdullayev-Sarvar <sarvar.abdullayev.0606@gmail.com>
2024-09-06 13:32:00 +0500

    fix profile

--- src/components/profileDesign/ProfileDesign.jsx
+++ src/components/profileDesign/ProfileDesign.jsx
@@ -1,10 +1,14 @@
 import { NavLink } from 'react-router-dom';
 import './ProfileDes.scss';
-import { useSelector } from'react-redux'; 
-
+import { useEffect, useState } from 'react';
+import axios from '../../api/axios'
 const Profile = () => {
-    const user = useSelector((state) => state.user);
+    const [user, setUser] = useState(null);
     
+    useEffect(() => {
+        axios('/users/2')
+            .then(response => setUser(response.data))
+    }, []);
     return (
         <div className='profile'>
             <div className='profile-container'>
@@ -13,12 +17,12 @@ const Profile = () => {
                     <>
                         <h1 className='profile-title'>Profile</h1>
                         <div className="profile-wrapper">
-                            <img className='profile-img' src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-png%2Favatar&psig=AOvVaw3rGjtJ7qOejcCJSeWuvEws&ust=1725688011131000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCPi4vfnOrYgDFQAAAAAdAAAAABAE' alt="Profile" />
+                            <img className='profile-img' src='https://static.vecteezy.com/system/resources/thumbnails/027/951/137/small_2x/stylish-spectacles-guy-3d-avatar-character-illustrations-png.png' alt="Profile" />
                             <div className='profile-info'>
-                                <h2 className='profile-name'>Name: {user.name[0]} {user.name[1]}</h2>
+                                <h2 className='profile-name'>Name: {user.name.firstname} {user.name.lastname}</h2>
                                 <p className='profile-id'>Your id: {user.username}</p>
                                 <p className='profile-email'>Email: {user.email}</p>
-                                <p className='profile-password'>Your password: {user.phone}</p>
+                                <p className='profile-password'>Your phone: {user.phone}</p>
                             </div>
                         </div>
                     </>
--- src/routes/RoutesController.jsx
+++ src/routes/RoutesController.jsx
@@ -1,14 +1,11 @@
 import { useRoutes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
 
 const Home = lazy(() => import('./Home/Home'));
 const Login = lazy(() => import('./login/Login'));
 const Profile = lazy(() => import('./profile/Profile'));
 
 const App = () => {
-  const token = useSelector((state) => state.token);
     return useRoutes([
       {
         path: '/',
@@ -20,7 +17,7 @@ const App = () => {
       },
       {
         path: '/profile',
-        element: token ? <Suspense fallback={<div className='w-full h-screen flex justify-center items-center'>Loading...</div>}><Profile /></Suspense> : <Navigate to="/login"/>,
+        element: <Suspense fallback={<div className='w-full h-screen flex justify-center items-center'>Loading...</div>}><Profile /></Suspense>,
       }
     ])
 };
